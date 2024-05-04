bff60fed03205b0790910b51fc294aeeaa68b37e
Sazzad ALI <sazzadatgithub1212@gmail.com>
2024-05-04 23:52:31 +1000

    Created a Hamburger menu for a smaller screen

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -5,14 +5,27 @@ import { navigation } from "../constants/";
 import Button from "./Button";
 import MenuSvg from "../assets/svg/MenuSvg";
 import { HamburgerMenu } from "./design/Header";
+import { useState } from "react";
 
 const Header = () => {
   const pathname = useLocation();
-  const [openNavigation, setNavigation] = useState(false);
+  const [openNavigation, setOpenNavigation] = useState(false);
+
+  const toggleNavigation = () => {
+    if (openNavigation) {
+      setOpenNavigation(false);
+    } else {
+      setOpenNavigation(true);
+    }
+  };
+
+  const handleClick = () => {
+    setOpenNavigation(false);
+  };
 
   return (
     <div
-      className={`fixed top-0 left-0 w-full z-50 bg-n-8/90 backdrop-blur-sm border-b border-n-6 lg:bg-n-8/90 lg:backdrop-blur-sm ${
+      className={`fixed top-0 left-0 w-full z-50 border-b border-n-6 lg:bg-n-8/90 lg:backdrop-blur-sm ${
         openNavigation ? "bg-n-8" : "bg-n-8/90 backdrop-blur-sm"
       }`}
     >
@@ -21,12 +34,17 @@ const Header = () => {
           <img src={brainwave} width={190} height={40} alt="Brainwave" />
         </a>
 
-        <nav className="hidden fixed top-[5rem] left-0 right-0 bottom-0 bg-n-8 lg:static lg:flex lg:max-auto lg:bg-transparent">
+        <nav
+          className={`${
+            openNavigation ? "flex" : "hidden"
+          } fixed top-[5rem] left-0 right-0 bottom-0 bg-n-8 lg:static lg:flex lg:max-auto lg:bg-transparent`}
+        >
           <div className="relative z-2 flex flex-col items-center justify-center m-auto lg:flex-row">
             {navigation.map((item) => (
               <a
                 key={item.id}
                 href={item.url}
+                onClick={handleClick}
                 className={`block relative font-code text-2xl uppercase text-n-1 transition-colors hover:text-color-1 ${
                   item.onlyMobile ? "lg:hidden" : ""
                 } px-6 py-6 md:py-8 lg:-mr-0.25 lg:text-xs lg:font-semibold ${
@@ -39,6 +57,8 @@ const Header = () => {
               </a>
             ))}
           </div>
+
+          <HamburgerMenu />
         </nav>
 
         <a
@@ -49,6 +69,14 @@ const Header = () => {
         </a>
         <Button className="hidden lg:flex" href="#login">
           Sign in
+        </Button>
+
+        <Button
+          className="ml-auto lg:hidden"
+          px="px-3"
+          onClick={toggleNavigation}
+        >
+          <MenuSvg openNavigation={openNavigation} />
         </Button>
       </div>
     </div>
