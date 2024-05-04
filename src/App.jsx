3f7f5dfc56befb031b62f370639aeda3dee47588
Sazzad ALI <sazzadatgithub1212@gmail.com>
2024-05-04 16:07:47 +1000

    finalised Reusable component - Button

--- src/App.jsx
+++ src/App.jsx
@@ -6,9 +6,7 @@ const App = () => {
     <>
       <h1 className="text-3xl font-bold underline">Hello world!</h1>
       <div className="pt-[4.75rem] lg:pt-[5.25rem] overflow-hidden">
-        <Button className="mt-10" href="#login">
-          Something
-        </Button>
+        <Button className="mt-10">Something</Button>
       </div>
       <ButtonGradient />
     </>
