ee5c49b2ccaacea409d9c6f9c775f3b5f7601fb1
Sazzad ALI <sazzadatgithub1212@gmail.com>
2024-05-04 21:33:51 +1000

    Uploaded external folder - Changes inside the components folder

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,12 +3,19 @@ import { useLocation } from "react-router-dom";
 import { brainwave } from "../assets/";
 import { navigation } from "../constants/";
 import Button from "./Button";
+import MenuSvg from "../assets/svg/MenuSvg";
+import { HamburgerMenu } from "./design/Header";
 
 const Header = () => {
   const pathname = useLocation();
+  const [openNavigation, setNavigation] = useState(false);
 
   return (
-    <div className="fixed top-0 left-0 w-full z-50 bg-n-8/90 backdrop-blur-sm border-b border-n-6 lg:bg-n-8/90 lg:backdrop-blur-sm">
+    <div
+      className={`fixed top-0 left-0 w-full z-50 bg-n-8/90 backdrop-blur-sm border-b border-n-6 lg:bg-n-8/90 lg:backdrop-blur-sm ${
+        openNavigation ? "bg-n-8" : "bg-n-8/90 backdrop-blur-sm"
+      }`}
+    >
       <div className="flex items-center px-5 lg:px-7.5 xl:px-10 max-lg:py-4">
         <a className="block w-[12rem] xl:mr-8" href="#hero">
           <img src={brainwave} width={190} height={40} alt="Brainwave" />
