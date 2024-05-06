ed6508e34b5e213af97ba13c40a76fd5bf33bd64
Sazzad ALI <sazzadatgithub1212@gmail.com>
2024-05-06 20:51:14 +1000

    Designed a Generating component

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -5,6 +5,7 @@ import { BackgroundCircles, BottomLine, Gradient } from "./design/Hero";
 import { heroIcons } from "../constants";
 import { ScrollParallax } from "react-just-parallax";
 import { useRef } from "react";
+import Generating from "./Generating";
 
 const Hero = () => {
   const parallaxRef = useRef(null);
@@ -53,6 +54,8 @@ const Hero = () => {
                   height={490}
                   alt="AI"
                 />
+
+                <Generating className="absolute left-4 right-4 bottom-5 md:left-1/2 md:right-auto md:bottom-8 md:w-[31rem] md:-translate-x-1/2" />
 
                 <ScrollParallax isAbsolutelyPositioned>
                   <ul className="hidden absolute -left-[5.5rem] bottom-[7.5rem] px-1 py-1 bg-n-9/40 backdrop-blur border border-n-1/10 rounded-2xl xl:flex">
